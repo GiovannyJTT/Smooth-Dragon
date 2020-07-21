45ce60d5c7f6d8020bf6d3025858db62672ca9ab
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2020-07-21 18:42:46 +0300

    added three-global.js and OrbitControls.js

---
+++ src/external-libs/threejs-0.118.3/three-global.js
@@ -0,0 +1,16 @@
+/**
+ * Library: Graphical Programming with ThreeJS (GPT)
+ * Author: Giovanny Javier Tipantuña Toapanta
+ * Email: giovanny.jtt@gmail.com
+ * Content: three-global.js
+ * 
+ * Description:
+ * 1. Importing three.js library as module from npm
+ * 2. Naming it "THREE" (following the nomenclature used in other three.js modules)
+ * 3. Exporting the object "THREE" from this script (module) to be imported in the rest (ex: in OrbitControls.js)
+ * 4. Then in "OrbitControls.js" (or other scripts) new functionality will be added to "THREE" object
+ */
+
+import * as THREE from 'three'
+
+export default THREE
