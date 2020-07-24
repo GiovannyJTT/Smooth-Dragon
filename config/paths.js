b3354710ea9a725b308edafb5787750885b32cdb
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2020-07-24 12:55:49 +0300

    added config/paths.js first version defining src, build and static

---
+++ config/paths.js
@@ -0,0 +1,7 @@
+import path from 'path';
+
+module.exports = {
+    src: path.resolve(__dirname, '../src'), // source files (libgptjs, main scripts)
+    build: path.resolve(__dirname, '../dist'), // production built files (compressed, images, html, etc.)
+    static: path.resolve(__dirname, '../resources') // assets to be copied to the build production folder
+}
