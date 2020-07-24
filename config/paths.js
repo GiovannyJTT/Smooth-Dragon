a727aaaafef81fc3300707a350479a6bfd0bc2fa
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2020-07-24 17:36:40 +0300

    config/paths.js: set static to '../scr/resources'

--- config/paths.js
+++ config/paths.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = {
     src: path.resolve(__dirname, '../src'), // source files (libgptjs, main scripts)
     build: path.resolve(__dirname, '../dist'), // production built files (compressed, images, html, etc.)
-    static: path.resolve(__dirname, '../resources') // assets to be copied to the build production folder
+    static: path.resolve(__dirname, '../src/resources') // assets to be copied to the build production folder
 }
