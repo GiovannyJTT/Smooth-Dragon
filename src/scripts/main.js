0c1e96995dd2c7656af04028fb0f476827febe64
GiovannyJTT <giovanny.jtt@gmail.com>
2022-05-24 20:01:07 +0200

    main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
 /**
- * Description: WebGL app using ThreeJS, Html5 and OOJS for exploring surface smoothing technique
  * Using library: Graphical Programming with ThreeJS (GPT)
  * Author: Giovanny Javier Tipantuña Toapanta
  * Email: giovanny.jtt@gmail.com
+ * Content:
+ *  main    entry point of this webGL app
+ *      WebGL app using ThreeJS, Html5 and OOJS for exploring surface smoothing technique
  */
 
 console.log("Loading GPT library")
+
 import configScene from './scene'
 import GPT_Renderer from '../libgptjs/GPT_Renderer'
 import GPT_App from '../libgptjs/GPT_App'
 
 // declared as "const" because: block-scoped, cannot be updated but its properties yes, cannot be redeclared
-console.log("configScene")
 const sce = configScene();
 const ren = new GPT_Renderer(window.innerWidth, window.innerHeight, sce);
 const app = new GPT_App(ren);
 
-console.log("app.init")
 app.init();
 
-console.log("app.run")
 app.run();
