019601d0f712cbf513fcb5b32e9c12da0d0e65d8
GiovannyJTT <giovanny.jtt@gmail.com>
2022-06-07 19:12:38 +0200

    src/index.js removed unnecessary comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import './styles/index.scss'
 
-// import this before the libgtpjs because it is adding functionality to object THREE
-// import './external-libs/threejs-0.118.3/OrbitControls'
-// import GPT_Model from './libgptjs/GPT_Model'
-// import GPT_Scene from './libgptjs/GPT_Scene'
-// import GPT_Render from './libgptjs/GPT_Renderer'
-// import GPT_APP from './libgptjs/GPT_App'
-
 import './scripts/main'
