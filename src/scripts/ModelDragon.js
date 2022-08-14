7a895f66391237b7f96abf3b1d4dc97dc99240b2
GiovannyJTT <giovanny.jtt@gmail.com>
2022-08-15 01:37:01 +0200

    ModelDragon.js computing UV for each triangle and attaching environment_map texture to simulate reflections

--- src/scripts/ModelDragon.js
+++ src/scripts/ModelDragon.js
@@ -42,48 +42,99 @@ ModelDragon.prototype.get_geometry = function () {
     // intemSize = 1 because there are 1 component per vertex-index
     geom.setIndex(new THREE.BufferAttribute(this.coords.edges_indices, 1));
 
+    // setting up the UV coordinates
+    const uvs = this.getUVs(geom);
+
+    // itemSize = 2 because each UV has 2 coordinates. uvs.lenght must be equalt to this.coords.edges_indices
+    geom.setAttribute(
+        "uv",
+        new THREE.BufferAttribute(uvs, 2)
+    );
+
+    // geom.computeVertexNormals();
+
+    geom.needsUpdate = true;
     return geom;
-}
-
-ModelDragon.prototype.getUVs = function(_geom){
-    
-    // computing reflection UVs for each face (triangle)
-    _geom.computeBoundingBox();
-    const max = geom.boundingBox.max;
-    const min = geom.boundingBox.min;
-    const displacement = new THREE.Vector2(0 - min.x, 0 - min.y);
-    const interval = new THREE.Vector2(max.x - min.x, max.y - min.y);
-
-    // each UV has 3 coordinates in this case, one UV per face (triangle)
-    const uvs = new Float32Array(2 * this.coords.triangles_indices.length);
-
-    for (let i = 0, n = 0; i < this.coords.triangles_indices.length; i++, n += 2){
-        const p1 = this.coords.points3d[ this.coords.triangles_indices[i].a ];
-        const p2 = this.coords.points3d[ this.coords.triangles_indices[i].b ];
-        const p3 = this.coords.points3d[ this.coords.triangles_indices[i].c ];
-
-        // pack all UV together for bufferAtrribute
-        uvs[n] = (p1.x + displacement.x) / interval.x;
-        uvs[n + 1] = (p1.y + displacement.x) / interval.y;
-    }
-
-    _geom.setAttribute("uv", new THREE.BufferAttribute(uvs, 2));
+    // at this points geom will be assigned into this.geometry
 }
 
 /**
  * Overriding in child object. It assumes `get_geometry` has been completed
+ * We are using a TextureCube as skybox, computing the reflections-uv and mapping them on the dragon surface
  */
 ModelDragon.prototype.get_material = function () {
+    // loading TextureCube as skybox
+    const _path = "./assets/images/Yokohama3/";
+    const _cube_sides_img = [
+        "posx.jpg",
+        "negx.jpg",
+        "posy.jpg",
+        "negy.jpg",
+        "posz.jpg",
+        "negz.jpg"
+    ];
+
+    const _texLoader = new THREE.CubeTextureLoader();
+    _texLoader.setPath(_path);
+    const _cubeTex = _texLoader.load(_cube_sides_img);
+
+    // creating material with all config
     const mat = new THREE.MeshPhongMaterial(
         {
             color: 0xe5ffe5, emissive: 0xb4ef3e, flatShading: false,
             specular: 0x003300, shininess: 70,
             side: THREE.FrontSide, transparent: true, opacity: 0.5,
-            // envMap: mapaEntorno
+            envMap: _cubeTex
         }
     );
 
+    mat.needsUpdate = true;
     return mat;
+}
+
+/**
+ * Calculates UV for planar surface (x, y, z) where z = 0.
+ * Computes the UV values for each face (triangle)
+ * Assumes `get_geometry()` is finished so input param `_geom` is used
+ * 
+ * @param {THREE.BufferGeometry} this.geometry already set
+ * @param {Array} this.coords.points3d reusing to compute uvs
+ * @param {Array} this.coords.triangles_indices reusing to compute uvs
+ * @return {Float32Array} Array containing all UVs for all faces
+ */
+ ModelDragon.prototype.getUVs = function(_geom){
+    if (_geom === undefined){
+        console.error("_geom undefined when getUvs()");
+        return;
+    }
+
+    _geom.computeBoundingBox();
+
+    const max = _geom.boundingBox.max;
+    const min = _geom.boundingBox.min;
+    const offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+    const range = new THREE.Vector2(max.x - min.x, max.y - min.y);
+
+    // each UV has 2 coordinates, each face (triangle) has 3 vertice, one UV per face
+    const _uvs = new Float32Array(6 * this.coords.triangles_indices.length);
+
+    for (let i = 0, n = 0; i < this.coords.triangles_indices.length; i++, n += 6){
+        const p1 = this.coords.points3d[ this.coords.triangles_indices[i].a ];
+        const p2 = this.coords.points3d[ this.coords.triangles_indices[i].b ];
+        const p3 = this.coords.points3d[ this.coords.triangles_indices[i].c ];
+
+        // pack all UV together for bufferAtrribute
+        _uvs[n] = (p1.x + offset.x) / range.x;
+        _uvs[n + 1] = (p1.y + offset.y) / range.y;
+
+        _uvs[n + 2] = (p2.x + offset.x) / range.x;
+        _uvs[n + 3] = (p2.y + offset.y) / range.y;
+
+        _uvs[n + 4] = (p3.x + offset.x) / range.x;
+        _uvs[n + 5] = (p3.y + offset.y) / range.y;
+    }
+
+    return _uvs;
 }
 
 export default ModelDragon;
