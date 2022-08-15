ff25925eaddc1073413b2828d0df493a4ea4da30
GiovannyJTT <giovanny.jtt@gmail.com>
2022-08-15 03:23:41 +0200

    Common.js containing values used in several places

---
+++ src/scripts/Common.js
@@ -0,0 +1,25 @@
+/**
+ * Values to be reused in several objects
+ */
+
+const FLOOR_WIDTH = 1000; // m
+const SKYBOX_WIDTH = 5 * FLOOR_WIDTH;
+
+const FLOOR_TEXTURE_PATH = "./assets/images/wood1.jpg";
+const SKYBOX_TEXTURE_PATH = "./assets/images/Yokohama3/";
+const SKYBOX_TEXTURE_IMAGES_NAMES = [
+    "posx.jpg",
+    "negx.jpg",
+    "posy.jpg",
+    "negy.jpg",
+    "posz.jpg",
+    "negz.jpg"
+];
+
+module.exports = {
+    FLOOR_WIDTH,
+    SKYBOX_WIDTH,
+    FLOOR_TEXTURE_PATH,
+    SKYBOX_TEXTURE_PATH,
+    SKYBOX_TEXTURE_IMAGES_NAMES
+}
