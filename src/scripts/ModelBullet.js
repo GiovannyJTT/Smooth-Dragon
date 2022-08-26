2050d22f135147e8a4205f16993a5e975961b527
GiovannyJTT <giovanny.jtt@gmail.com>
2022-08-26 22:46:21 +0200

    ModelBullet implemented move_to_next_point

--- src/scripts/ModelBullet.js
+++ src/scripts/ModelBullet.js
@@ -4,10 +4,25 @@ import GPT_Model from "../libgptjs/GPT_Model";
 /**
  * Creates a model for bullet
  * Inherits from GPT_Model
+ * @param {[THREE.Vector3]} trajectory_points3D_ array of Vector3 points to move along the bullet
  */
-function ModelBullet () {
+function ModelBullet (trajectory_points3D_) {
     // 1. Call parent object
     GPT_Model.call(this);
+
+    this.trajectory_points3D = trajectory_points3D_
+
+    if (undefined === this.trajectory_points3D) {
+        console.error("ModelBullet: trajectory_points is undefined");
+        return;
+    }
+
+    if (this.trajectory_points3D.size == 0) {
+        console.error("ModelBullet: trajectory_points has 0 elements");
+        return;
+    }
+
+    this.current_point_index = 0;
 }
 
 // 2. Extend from parent object prototype (keep proto clean)
@@ -20,7 +35,7 @@ ModelBullet.prototype.constructor = ModelBullet;
  * Overriding it
  */
 ModelBullet.prototype.get_geometry = function () {
-    const _geom = new THREE.SphereGeometry(20, 10, 10);
+    const _geom = new THREE.SphereGeometry(22, 8, 8);
     return _geom;
 }
 
@@ -32,7 +47,7 @@ ModelBullet.prototype.get_material = function () {
         {
             color: 0xD76009,
             emissive: 0x681F77,
-            FlatShading: false,
+            flatShading: true,
             specular: 0xFF5733,
             shininess: 50,
             side: THREE.FrontSide,
@@ -50,6 +65,21 @@ ModelBullet.prototype.dispose_buffers = function () {
     this.geometry = null;
     this.material = null;
     this.mesh = null;
+}
+
+ModelBullet.prototype.move_to_next_point = function () {
+    const _last_index = this.trajectory_points3D.length - 1;
+
+    if (this.current_point_index < _last_index) {
+        this.current_point_index++;
+        const _p = this.trajectory_points3D[this.current_point_index];
+        this.mesh.position.set(_p.x, _p.y, _p.z);
+
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 export default ModelBullet;
