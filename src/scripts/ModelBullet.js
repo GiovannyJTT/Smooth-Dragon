efe332e50c2f2e144720a5510aced68448bf4955
GiovannyJTT <giovanny.jtt@gmail.com>
2022-08-26 23:34:50 +0200

    ModelBullet implemented move_to_next_point_interpolated()

--- src/scripts/ModelBullet.js
+++ src/scripts/ModelBullet.js
@@ -1,3 +1,4 @@
+import { lerp } from "three/src/math/MathUtils";
 import THREE from "../external-libs/threejs-0.118.3/three-global";
 import GPT_Model from "../libgptjs/GPT_Model";
 
@@ -23,6 +24,7 @@ function ModelBullet (trajectory_points3D_) {
     }
 
     this.current_point_index = 0;
+    this.prev_ts = performance.now();
 }
 
 // 2. Extend from parent object prototype (keep proto clean)
@@ -67,17 +69,62 @@ ModelBullet.prototype.dispose_buffers = function () {
     this.mesh = null;
 }
 
+/**
+ * Increases "current_point_index" and moves mesh to next point
+ * @returns {Bool} false when index >= last_index, true otherwise
+ */
 ModelBullet.prototype.move_to_next_point = function () {
+
     const _last_index = this.trajectory_points3D.length - 1;
+    if (this.current_point_index >= _last_index) {
+        return false;
+    }
+    
+    this.current_point_index++;
+    const _p = this.trajectory_points3D[this.current_point_index];
+    this.mesh.position.set(_p.x, _p.y, _p.z);
+    
+    return true;
+}
 
-    if (this.current_point_index < _last_index) {
-        this.current_point_index++;
+/**
+ * Based on time passed since last call and Common.BULLET_STEP_DURATION_MS (duration
+ *      between 2 succesive points3D)
+ * 
+ * While elapsed < BULLET_SEPT_DURATION_MS it calculates interpolated point and moves the object
+ * Otherwise it only increases the current_point_index
+ * 
+ * @returns {Bool} true object moved, false otherwise
+ */
+ModelBullet.prototype.move_to_next_point_interpolated = function () {
+    
+    const _last_index = this.trajectory_points3D.length - 1;
+    if (this.current_point_index >= _last_index) {
+        return false;
+    }
+
+    const BULLET_STEP_DURATION_MS = 100;
+    const _now_ts = performance.now();
+    const _elapsed = _now_ts - this.prev_ts;
+
+    if (_elapsed < BULLET_STEP_DURATION_MS) {
+        const _i = _elapsed / BULLET_STEP_DURATION_MS;
+
         const _p = this.trajectory_points3D[this.current_point_index];
-        this.mesh.position.set(_p.x, _p.y, _p.z);
-
+        const _p_next = this.trajectory_points3D[this.current_point_index + 1];
+    
+        // interpolate coordinates between current and next
+        const _ip_x = lerp(_p.x, _p_next.x, _i);
+        const _ip_y = lerp(_p.y, _p_next.y, _i);
+        const _ip_z = lerp(_p.z, _p_next.z, _i);
+    
+        // apply
+        this.mesh.position.set(_ip_x, _ip_y, _ip_z);
         return true;
     }
     else {
+        this.current_point_index++;
+        this.prev_ts = performance.now();
         return false;
     }
 }
