99110f7dd86729ad36c4c8e966ad2fa07773244e
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2022-09-14 01:58:41 +0200

    particles started

---
+++ src/scripts/DragonFire.js
@@ -0,0 +1,9 @@
+/**
+ * @module libgptjs Graphical Programming with ThreeJS (GPT)
+ * @class DragonFire using particle system from Neebula-threejs
+ */
+
+import Nebula, {SpriteRenderer} from "three-nebula"
+import dfp from "./DragonFireParticles.json"
+
+console.log(dfp)
