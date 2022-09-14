60cab74d87f2b5eff9fa70c64a695d5a0786b9df
GiovannyJTT <giovanny.jtt@gmail.com>
2022-09-14 09:31:08 +0200

    DragonFire.js update_to_dragon_mouth() applying operations in order

--- src/scripts/DragonFire.js
+++ src/scripts/DragonFire.js
@@ -3,7 +3,7 @@
  * @class DragonFire using particle system from Neebula-threejs
  */
 
-import Nebula, {SpriteRenderer} from "three-nebula"
+import Nebula, { SpriteRenderer } from "three-nebula"
 import dfp from "./DragonFireParticles.json"
 import THREE from "../external-libs/threejs-0.118.3/three-global";
 
@@ -16,7 +16,7 @@ import THREE from "../external-libs/threejs-0.118.3/three-global";
  * 
  * @param {THREE.Scene} scene_ reference to the main threejs scene to be used into the particles system
  */
-function DragonFire (scene_) {
+function DragonFire(scene_) {
 
     if (undefined === scene_) {
         console.error("DragonFire: scene is undefined");
@@ -38,46 +38,62 @@ function DragonFire (scene_) {
  * 
  * 1. Dragon is rotating in its vertical Y axis only, so we can calculate the corresponding mouth position in XZ plane
  * 2. It updates emitter position taking into account current dragon rotation
+ *      a. Move model to origin
+ *      b. Align with axis (un-do the current rotation)
+ *      c. Calculate mouth position when model is at origin
+ *      d. Apply rotation to mouth at origin
+ *      e. Translate coordinates back to current model position 
  * 3. We rotate the emitter according to dragon (so emission vector does not keep pointing to the same direction)
  * 
  * @param {THREE.Vector3} dragon_pos_ current world position of dragon
  * @param {THREE.Vector3} dragon_rot_ current world rotation of dragon
  */
 DragonFire.prototype.update_to_dragon_mouth = function (dragon_pos_, dragon_rot_) {
-
     if (undefined !== this.nebula) {
-        this.nebula.emitters.forEach( (emitter_) => {
+        this.nebula.emitters.forEach((emitter_) => {
 
-            const _pos = new THREE.Vector3();
+            // move model to origin
+            const _dragon_at_origin = new THREE.Vector3(
+                -dragon_pos_.x,
+                -dragon_pos_.y,
+                -dragon_pos_.z
+            );
 
-            // update emitter position
-            const _sin_a = Math.sin(dragon_rot_.y);
-            const _cos_a = Math.cos(dragon_rot_.y);
+            // align with axis (un-do current rotation)
+            const _dragon_at_origin_aligned = new THREE.Vector3().copy(_dragon_at_origin)
+                .applyAxisAngle(
+                    new THREE.Vector3(0, 1, 0), -dragon_rot_.y
+                );
 
-            // clockwise rotation
-            if (dragon_rot_.y > 0) {
-                _pos.x = _cos_a * dragon_pos_.x + _sin_a * dragon_pos_.z;
-                _pos.z = - _sin_a * dragon_pos_.x + _cos_a * dragon_pos_.z;
-            }
-            // counter-clockwise rotation
-            else if (dragon_rot_.y < 0) {
-                _pos.x = _cos_a * dragon_pos_.x - _sin_a * dragon_pos_.z;
-                _pos.z = _sin_a * dragon_pos_.x + _cos_a * dragon_pos_.z;
-            }
+            // calculate mouth position when model is at origin
+            const _mouth_at_origin = new THREE.Vector3(
+                _dragon_at_origin_aligned.x - 120,
+                _dragon_at_origin_aligned.y + 240,
+                _dragon_at_origin_aligned.z
+            );
 
-            // rectify
-            _pos.x = dragon_pos_.x + _pos.x;
-            _pos.z = dragon_pos_.z + _pos.z;
-            _pos.y = dragon_pos_.y + 240;
+            // apply rotation to mouth at origin
+            const _mouth_at_origin_rotated = new THREE.Vector3().copy(_mouth_at_origin)
+                .applyAxisAngle(
+                    new THREE.Vector3(0, 1, 0), dragon_rot_.y
+                );
 
-            emitter_.position = _pos;
+            // translate coordinates back to current model position
+            const _mouth_translated = new THREE.Vector3(
+                _mouth_at_origin_rotated.x + dragon_pos_.x * 2,
+                _mouth_at_origin_rotated.y + dragon_pos_.y * 2,
+                _mouth_at_origin_rotated.z + dragon_pos_.z * 2,
+            );
 
-            // update emitter rotation
-            emitter_.rotation.x = 0;
-            emitter_.rotation.y = dragon_rot_.y;
-            emitter_.rotation.z = 0;
+            emitter_.position = _mouth_translated;
+
+            // update emitter rotation: fix 90 deg rotation to point mouth-forward
+            emitter_.rotation.x = dragon_rot_.x;
+            emitter_.rotation.y = dragon_rot_.y - 1.5707963267948966;
+            emitter_.rotation.z = dragon_rot_.z;
+
         });
-        
+
         this.nebula.update();
     }
 }
